0287cb8b2569c819d38670250f4300663ae8a270
Aryan Sapra <aryansaprajiit@gmail.com>
2025-04-12 21:18:12 +0530

    Java basics

---
+++ javabasics/ScannerInput.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+import java.util.Scanner;
+
+public class ScannerInput {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int input1 = scanner.nextInt();
+        System.out.println("First input is:" + input1);
+    }
+}
---
+++ javabasics/StringToNumeric.java
@@ -0,0 +1,8 @@
+package javabasics;
+
+public class StringToNumeric {
+    public static void main(String args[]) {
+        String val = "1999";
+        System.out.print("date of birth is = " + Integer.parseInt(val));
+    }
+}
